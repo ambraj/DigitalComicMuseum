48c6f6d70d0c9dee147494ab2a87f66b5e72f756
Rajat Rajput <rajputrajat@gmail.com>
2015-10-15 01:30:43 +0530

    merge card vied activity from Ambuj's project

---
+++ app/src/main/java/com/quarkstar/digitalcomicmusium/adapter/ComicData.java
@@ -0,0 +1,31 @@
+package com.quarkstar.digitalcomicmusium.adapter;
+
+public class ComicData {
+//    int icon;
+    String imageUrl;
+    String gameName;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setgameName(String name) {
+        this.gameName = name;
+    }
+
+//    public int getIcon() {
+//        return icon;
+//    }
+//
+//    public void setIcon(int icon) {
+//        this.icon = icon;
+//    }
+}
